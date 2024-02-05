be3633dd4182e80f200d2c091ecabb5070b53cc3
DMC Industries <dmcrampton02@gmail.com>
2024-02-04 21:51:55 -0500

    Initial Commit

---
+++ ToneTrek/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToneTrek
+//
+//  Created by user256714 on 2/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToneTrek/ToneTrekApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneTrekApp.swift
+//  ToneTrek
+//
+//  Created by user256714 on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ToneTrekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
